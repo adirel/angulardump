77d9f69cce42066a27e5d76e7a587d96db2ecdf4
אדיר אלפרסי <adir_el@jer.ad.malam.com>
2017-12-11 15:38:51 +0200

    part3 course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+  <h1> {{ title }} </h1>
+  <div *ngIf="myArr; then trueTmpl; else otherTmpl">I exist</div>
+
+  <ng-template #otherTmpl>No, I do.</ng-template>
+  <ng-template #trueTmpl>Yes Ido!.</ng-template>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'course';
+  myObject = {
+    gender: 'male',
+    age: 33,
+    location: 'USA'
+  };
+
+  myArr = true;
 }
+
